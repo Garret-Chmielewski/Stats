2c49d9c1d70c613c7110ac3573067cc601bd64aa
CybErr404 <114787759+CybErr404@users.noreply.github.com>
2024-02-02 20:54:30 -0500

    Update PokemonArean.java
    Added Chimchar data in the Pokémon Arena class for our screenshot assignment.

--- classwork/src/pokemon/PokemonArean.java
+++ classwork/src/pokemon/PokemonArean.java
@@ -2,5 +2,11 @@ package pokemon;
 
 public class PokemonArean 
 {
+	//Code added by Mia W. for screenshot commits. C:
+	Chimchar chimcharMain = new Chimchar();
+        Chimchar chimcharTarget = new Chimchar();
+	System.out.println("Chimchar, USE SCRATCH!");
+	chimcharMain.attackOne(chimcharTarget);
+        System.out.println("Chimchar Target Hp: " + chimcharTarget.getHp());
 	
 }
